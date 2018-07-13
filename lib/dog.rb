a1b2f539c41bb9689a36cc9a5812af8421c6e0af
madhuv2002 <madhuv2002@gmail.com>
2018-07-13 18:49:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,23 +4,7 @@ class Dog
   attr_reader :name, :breed
   attr_writer :name, :breed
 end
-#   def name
-#     @name
-#   end 
-  
-  
-#   def name= (name)
-#     @name = name 
-#   end 
-  
-#   def breed
-#     @breed
-#   end
-  
-#   def breed= (breed)
-#     @breed = breed
-#   end 
-# end
+
     
     
     
